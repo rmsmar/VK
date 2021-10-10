c4b8896b4ce6d6e7558ae46e4ec7678cf50562ee
rmsmar <rms.marina89@gmail.com>
2021-10-10 23:06:57 +0300

    commit_v2

--- src/main/java/ru/netology/domain/Donut.java
+++ src/main/java/ru/netology/domain/Donut.java
@@ -3,7 +3,7 @@ package ru.netology.domain;
 public class Donut {
     private boolean isDonut;
     private int paidDuration;
-
+    private Placeholder placeholder;
     private boolean canPublishFreeCopy;
     private String editMode;
 
